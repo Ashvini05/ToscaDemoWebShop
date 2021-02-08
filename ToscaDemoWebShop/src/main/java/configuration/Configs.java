8cc581ac7885630d336b0e7b666c95365407ac18
nagar <nagar@LAPTOP-UME8H5R5.Dlink>
2021-02-08 10:26:13 +0530

    added configs

---
+++ ToscaDemoWebShop/src/main/java/configuration/Configs.java
@@ -0,0 +1,9 @@
+package configuration;
+
+public class Configs {
+	public static String driverPath = "Drivers\\\\ChromeDriver\\\\chromedriver.exe";
+	public static String uri = "http://demowebshop.tricentis.com/";
+	public static int timeOut = 30;
+	public String name;
+	
+}
